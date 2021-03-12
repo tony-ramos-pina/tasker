28e544b252f5f8c84a5583c52e97569c54dcb384
tony-ramos-pina <tony.ramos.pina@gmail.com>
2021-03-12 10:35:48 +0000

    alterações após PR branch 12

--- tasker/app/helpers/tasks_helper.rb
+++ tasker/app/helpers/tasks_helper.rb
@@ -1,10 +1,8 @@
 module TasksHelper
-
     def linked_tag_list(task)
         list = task.tag_list.map do |tag_name|
             link_to tag_name, searchByTag_task_path(task, tag: tag_name)
         end
-        safe_join list, ", "
+        safe_join list, (", ")
     end
-
 end
